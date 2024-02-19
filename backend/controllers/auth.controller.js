68a58a82d7a8a8885980419e8fa0590324850bc9
Satishghode <satishghode9075@gmail.com>
2024-02-19 22:55:29 +0530

    Complated the user SignUp api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,18 +1,56 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+
 // signup function
 export const signup = async (req, res) => {
   try {
-    
+    const { fullName, userName, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "password don't matach" });
+    }
+
+    const user = await User.findOne({ userName });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    // HASH PASSWORD HERE
+    const salt = await bcrypt.genSalt(12);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
+
+    const newUser = new User({
+      fullName,
+      userName,
+      password: hashPassword,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    if (newUser) {
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        userName: newUser.userName,
+        gender: newUser.gender,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: " Invalid user Data " });
+    }
   } catch (error) {
-    
+    console.log("Error in signUp Controller", error.message);
+    res.status(500).json({ error: "Internal server Error" });
   }
 };
 
-// login function 
-export const login = (req, res) => {
-  
-};
+// login function
+export const login = (req, res) => {};
 
-// logout function 
-export const logout = (req, res) => {
-  
-};
+// logout function
+export const logout = (req, res) => {};
