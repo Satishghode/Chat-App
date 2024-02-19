10afa696e869cf02d3fadbc60515ab92e7ead669
Satishghode <satishghode9075@gmail.com>
2024-02-19 20:59:55 +0530

    Connect to MongoDB

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,18 @@
 // signup function
-export const signup = (req, res) => {
-  res.send("finally called the signup function ");
+export const signup = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
 };
 
 // login function 
 export const login = (req, res) => {
-  res.send("finally called the login method ");
+  
 };
 
 // logout function 
 export const logout = (req, res) => {
-  res.send("finally called the logout function ");
+  
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routers/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`server Running on port : ${PORT} `);
 });
 
