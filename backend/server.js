e98956e734362f31957f0686f5fa6dff182dc697
Satishghode <satishghode9075@gmail.com>
2024-02-19 21:22:58 +0530

    allow the Json and fromat the server js file

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+
+
 import authRouter from "./routers/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 3000;
 
+dotenv.config();
+
+app.use(express.json()); // to parese the incoming request with json payload (from req.body); 
+
+// if any Browser hit the URL http://localhost:5000/api/auth automatics call the authRouter Function/methos present in the routes folder
+app.use("/api/auth", authRouter);
+
+// create an connection to MongoDB.
 app.listen(PORT, () => {
   connectToMongoDB();
   console.log(`server Running on port : ${PORT} `);
 });
 
-// if any Browser hit the URL http://localhost:5000/api/auth automatics call the authRouter Function/methos present in the routes folder
-app.use("/api/auth", authRouter);
 
-app.get("/", (req, res) => {
-  // root route for http://localhosr:5000
-  res.send("server is listening ??? ");
-});
