f3a1df399ec47b2fbc2a8dfeafbe7b3ffd31f14e
Satishghode <satishghode9075@gmail.com>
2024-02-19 17:00:34 +0530

    SetUp dotenv file for the backend

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv'
 
 const app = express();
+dotenv.config();
 
-app.listen(5000 ,() => {
-    console.log(`server Running on port 5000 `);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT ,() => {
+    console.log(`server Running on port : ${PORT} ` );
 })
 
 
