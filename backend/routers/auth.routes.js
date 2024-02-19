2c5dd62ac39821e9043000117d6800f9d9fcd655
Satishghode <satishghode9075@gmail.com>
2024-02-19 18:45:13 +0530

    change the requests

--- backend/routers/auth.routes.js
+++ backend/routers/auth.routes.js
@@ -4,10 +4,10 @@ import { login, logout, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 //  if any request comes from the /api/auth/login authomatocity call the loginUse method/function
-router.get("/signup", signup);
+router.post("/signup", signup);
 
-router.get("/login", login);
+router.post("/login", login);
 
-router.get("/logout", logout);
+router.post("/logout", logout);
 
 export default router;
