933b13056c45c1ada66be2aeedebdce49ae56956
Satishghode <satishghode9075@gmail.com>
2024-02-21 11:49:52 +0530

    Complated message models

---
+++ backend/models/message.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose"
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: String,
+        ref : "User",
+        required: true,
+    },
+    receiverId : {
+        type : String,
+        ref : "User",
+        required : true,
+    },
+    message : {
+        type : String,
+        required : true,
+    }
+
+},{ timestamps: true })
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
