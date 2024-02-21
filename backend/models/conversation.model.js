6e84c5626d4d00539924b226701ea9fb3e6b53da
Satishghode <satishghode9075@gmail.com>
2024-02-21 14:01:54 +0530

    complated the conversation Schema model

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const conversationSchema = new mongoose.Schema({
+    participates : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User",
+        },
+    ],
+    messages : [
+        {
+            type : mongoose.Schema.types.ObjectId,
+            ref : "Message",
+            default : [],
+        },
+    ],
+},{ timestamps : true });
+
+const conversation = mongoose.model("Conversation", conversationSchema);
+
+export default conversation;
