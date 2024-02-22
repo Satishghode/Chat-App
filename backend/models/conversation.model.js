a4c6ca48ae5ca8a3489108e959616b1cb4b0a756
Satishghode <satishghode9075@gmail.com>
2024-02-23 01:56:33 +0530

    typo misstake inside consversation model

--- backend/models/conversation.model.js
+++ backend/models/conversation.model.js
@@ -9,7 +9,7 @@ const conversationSchema = new mongoose.Schema({
     ],
     messages : [
         {
-            type : mongoose.Schema.types.ObjectId,
+            type : mongoose.Schema.Types.ObjectId,
             ref : "Message",
             default : [],
         },
