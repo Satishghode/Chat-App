36529cd0ba42a66faf0df33a9a86f2c28e3aa0c3
Satishghode <satishghode9075@gmail.com>
2024-02-23 01:56:52 +0530

    completed sent message api

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,41 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
 
 
-export const sentMessage = (req, res) => {
+export const sentMessage = async (req, res) => {
+    try {
+        const {message} = req.body; // get the message from the request Object.
+        const {id : receiverId   } = req.params; 
+        const senderId = req.user._id;
+
+        let conversation = await Conversation.findOne({
+            participates : { $all: [senderId, receiverId] },
+        })
+
+        if(!conversation){
+            conversation = await Conversation.create({
+                participates : [senderId, receiverId]
+            });
+        }
+
+        const newMessage = new Message( {
+            senderId ,
+            receiverId,
+            message,
+        })
+
+        if(newMessage){
+            conversation.messages.push(newMessage._id);
+        }
+
+        await conversation.save();
+        await newMessage.save();
+
+        res.status(201).json(newMessage)
+        
+    } catch (error) {
+        console.log('error in sendMessage Controller', error.message );
+        res.status(500).json({ error : " Internal Server Error " })
+    }
     
 }
