b17c2f8fc0a2a87baeee34bbaeda4f5208da166f
Satishghode <satishghode9075@gmail.com>
2024-02-24 17:15:25 +0530

    add commite messages to the sentMessage function user

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -2,35 +2,39 @@ import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 
 
+// sent message Functions user can sent the messages 
 export const sentMessage = async (req, res) => {
     try {
         const {message} = req.body; // get the message from the request Object.
         const {id : receiverId   } = req.params; 
         const senderId = req.user._id;
-
+        // it checks the receiverId and senderId present or not if it present the save the id 
         let conversation = await Conversation.findOne({
             participates : { $all: [senderId, receiverId] },
         })
-
+        // if there is no conversation. then create th new conversation object
         if(!conversation){
             conversation = await Conversation.create({
                 participates : [senderId, receiverId]
             });
         }
-
+        // create the new message Object and stored in the newMessage object 
         const newMessage = new Message( {
             senderId ,
             receiverId,
             message,
         })
-
+        //if there is messages present the add this messages.id in inside the conversation.messages 
         if(newMessage){
             conversation.messages.push(newMessage._id);
         }
 
+
+
         // await conversation.save();
         // await newMessage.save();
-
+        
+        // Save the data at same time both models 
         await Promise.all([conversation.save(), newMessage.save()]);
 
         res.status(201).json(newMessage)
