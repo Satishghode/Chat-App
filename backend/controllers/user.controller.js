b73c7de4cf89c7c845d5251455b970ba65ff5470
Satishghode <satishghode9075@gmail.com>
2024-03-03 13:20:46 +0530

    Complated the getUsersFormSidebar functionality

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,11 +1,16 @@
+import User from "../models/user.model.js";
 
 // this is an getUserFormSiderbar 
-export const getUsersFormSidebar = (req, res,) => {
+export const getUsersFormSidebar = async (req, res,) => {
     try {
-        console.log('call form getUsersFormSidebar');
+        const loggedInUserId = req.user._id;
+
+        const filteredUsers = await User.find({ _id: { $ne: loggedInUserId }}).select('-password') ;
+
+        res.status(200).json(filteredUsers);
         
     } catch (error) {
         console.log('error in getMessage controller', error.message);
         res.status(500).json({ error : " Internal server Error " });
     }
-}
+}   
