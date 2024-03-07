4dc1e3ee9c38ab79df62440b57ea34523233076f
Satishghode <satishghode9075@gmail.com>
2024-03-07 21:54:33 +0530

    Create the pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,9 @@
+import Login from "./pages/login/Login";
+
 function App() {
   return (
-    // try and bulid static pages in the same directory as static pages
-    <div className="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img
-          src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
+    <div className="p-4 h-screen flex items-center justify-center " >
+      <Login />
     </div>
   );
 }
