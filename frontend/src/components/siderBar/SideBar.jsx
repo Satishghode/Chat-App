a2d4b73e85e0ed5e1ceaee089d9df9e80868ca64
Satishghode <satishghode9075@gmail.com>
2024-03-08 02:37:37 +0530

    Complated the logoutButton Component

--- frontend/src/components/siderBar/SideBar.jsx
+++ frontend/src/components/siderBar/SideBar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import SearchInput from './SearchInput'
 import Conversations from './Conversations'
+import LogoutButton from './LogoutButton'
 
 function SideBar() {
   return (
-    <div>
+    <div className='border-r border-slate-600 p-4  flex flex-col '>
       <SearchInput />
-
       <div className='divider px-3' ></div>
       <Conversations />
-      {/* <LogOutButton /> */}
+      <LogoutButton />
 
     </div>
   )
