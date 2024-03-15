7d9a25ce0e085bd2071a9f78b551dbff68ed9740
Satishghode <satishghode9075@gmail.com>
2024-03-15 20:11:59 +0530

    setUp UseLogin method

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,7 +1,13 @@
 
-
+// this is an useLogin method get the data from the server and compare it with the current user data.
 const useLogin = () => {
-    
+    try {
+        
+    } catch (error) {
+        console.log("this erro for useLOfIN + " + error.message);
+
+
+    }
 
 }
 
