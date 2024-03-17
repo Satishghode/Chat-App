acc76cb3dcb80d0efe9e93d698ddbc11805ec8fd
Satishghode <satishghode9075@gmail.com>
2024-03-17 21:50:11 +0530

    Complated the ogoutButton funcationality

--- frontend/src/components/siderBar/LogoutButton.jsx
+++ frontend/src/components/siderBar/LogoutButton.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
 import { CiLogout } from "react-icons/ci";
+import useLogout from "../../hooks/useLogout";
 
 function LogoutButton() {
+  const { loading, logout } = useLogout();
   return (
-    <div className='mt-auto' >
-        <CiLogout className='h-6 w-6 text-white cursor-pointer ' />
+    <div className="mt-auto">
+      {!loading ? (
+        <CiLogout className="h-6 w-6 text-white cursor-pointer   "  onClick={logout} />
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
     </div>
-  )
+  );
 }
 
-export default LogoutButton
+export default LogoutButton;
