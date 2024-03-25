07cc08404cbfcc6aac2666c9011efcb2fb129fa2
Satishghode <satishghode9075@gmail.com>
2024-03-25 07:29:36 +0530

    Get all conversations from the DB and show inside the Slider

--- frontend/src/components/siderBar/Conversation.jsx
+++ frontend/src/components/siderBar/Conversation.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 
-function Conversation() {
-  var datetime = new Date();
-  var time = datetime.toISOString().slice(0,10);
+function Conversation(  {conversation, lastIdx } ) {
+  
   return (
     <>
       <div className="flex gap-2 items-center hover:bg-gray-800 rounded-lg p-2 py-1 cursor-pointer ">
         {/* <button className="btn btn-secondary">Secondary</button> */}
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src={conversation.profilePic} />
           </div>
         </div>
         <div className=" flex flex-col flex-1 " >
           <div className=" flex gap-3 justify-between "  >
-            <p className="font-bold text-gray-200 " >Satish Ghode</p>
-            <span className="text-sm" >{time}</span>
+            <p className="font-bold text-gray-200 " >{ conversation.userName }</p>
+            <span className="text-sm" >{  }</span>
           </div>
 
         </div>
